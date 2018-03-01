2ed89eaad1fb386996bc99d5a297964aa57e2680
Ankita Khurana <ankita0096@gmail.com>
2018-03-01 12:47:55 +0530

    es2015

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const express = require('express');
 const app = express();
 app.use('/', express.static(__dirname + "/static"));
 
-app.listen(3001,function () {
+app.listen(3000,function () {
     
 });
